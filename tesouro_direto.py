85a0bd93c7f4f184435dcc765d703852f9ffe5a5
Denis Germano <dlcgermano@gmail.com>
2018-10-28 00:17:38 -0300

    Applying black code style

--- tesouro_direto.py
+++ tesouro_direto.py
@@ -10,13 +10,14 @@ from datetime import datetime
 from io import StringIO
 from prettytable import PrettyTable
 
-INFO = \
-    "Histórico de preços e taxas (http://www.tesouro.gov.br/tesouro-direto-balanco-e-estatisticas)\n\n"\
-    "Tesouro Selic (LFT)\n"\
-    "Tesouro IPCA+ (NTN-B Principal)\n"\
-    "Tesouro IPCA+ com Juros Semestrais (NTN-B)\n"\
-    "Tesouro Prefixado (LTN)\n"\
+INFO = (
+    "Histórico de preços e taxas (http://www.tesouro.gov.br/tesouro-direto-balanco-e-estatisticas)\n\n"
+    "Tesouro Selic (LFT)\n"
+    "Tesouro IPCA+ (NTN-B Principal)\n"
+    "Tesouro IPCA+ com Juros Semestrais (NTN-B)\n"
+    "Tesouro Prefixado (LTN)\n"
     "Tesouro Prefixado com Juros Semestrais (NTN-F)\n"
+)
 
 URL = "http://www.tesouro.fazenda.gov.br/tesouro-direto-precos-e-taxas-dos-titulos"
 URL_HISTORY = "http://sisweb.tesouro.gov.br/apex/f?p=2031:2:::::"
@@ -24,19 +25,26 @@ TREASURE_LIST = ["LFT", "LTN", "NTN-C", "NTN-B", "NTN-B Principal", "NTN-F"]
 TREASURE_LINK = {treasure: {} for treasure in TREASURE_LIST}
 
 
-parser = argparse.ArgumentParser(description='Tesouro Direto')
+parser = argparse.ArgumentParser(description="Tesouro Direto")
 parser.add_argument(
-    '-t', '--title',
-    type=str, choices=TREASURE_LIST, help='Uma string representando o código do título')
+    "-t",
+    "--title",
+    type=str,
+    choices=TREASURE_LIST,
+    help="Uma string representando o código do título",
+)
 parser.add_argument(
-    '-i', '--info',
-    action='store_true', help='Informações sobre o código de cada tesouro')
+    "-i",
+    "--info",
+    action="store_true",
+    help="Informações sobre o código de cada tesouro",
+)
+parser.add_argument("-y", "--year", type=int, help="Um inteiro representando o ano")
 parser.add_argument(
-    '-y', '--year',
-    type=int, help="Um inteiro representando o ano")
-parser.add_argument(
-    '-d', '--download',
-    action='store_true', help="Adicione para ser feito o download do gráfico"
+    "-d",
+    "--download",
+    action="store_true",
+    help="Adicione para ser feito o download do gráfico",
 )
 
 
@@ -44,21 +52,21 @@ def update_treasure_link():
     global TREASURE_LINK
 
     response = requests.get(URL_HISTORY)
-    content = response.content.decode('utf-8')
+    content = response.content.decode("utf-8")
     soup = bs(content, "html.parser")
-    table = soup.find('div', class_="bl-body")
+    table = soup.find("div", class_="bl-body")
 
-    year_list = [span.text[:4] for span in table.find_all('span')]
+    year_list = [span.text[:4] for span in table.find_all("span")]
     year_index = 0
     for data in table:
-        if data.name == 'br':
+        if data.name == "br":
             continue
-        if data.name == 'span':
+        if data.name == "span":
             if year_list[year_index] in data.text:
                 year = int(year_list[year_index])
                 year_index += 1
-        if data.name == 'a':
-            href = data.get('href')
+        if data.name == "a":
+            href = data.get("href")
             TREASURE_LINK[data.text][year] = f"http://sisweb.tesouro.gov.br/apex/{href}"
 
 
@@ -81,47 +89,49 @@ def get_treasure_history_data(title, year, download):
     print(book[sheet_name].content)
 
     df = pd.read_csv(StringIO(book[sheet_name].csv))
-    df.rename(columns={
-        'Vencimento': 'DATA',
-        'Unnamed: 5': 'PU_BASE'
-    }, inplace=True)
-    df['DATA'] = pd.to_datetime(df['DATA'][1:], format='%d/%m/%Y')
-    df['PU_BASE'] = pd.to_numeric(df['PU_BASE'][1:])
-    df.sort_values('DATA', ascending=False, inplace=True)
+    df.rename(columns={"Vencimento": "DATA", "Unnamed: 5": "PU_BASE"}, inplace=True)
+    df["DATA"] = pd.to_datetime(df["DATA"][1:], format="%d/%m/%Y")
+    df["PU_BASE"] = pd.to_numeric(df["PU_BASE"][1:])
+    df.sort_values("DATA", ascending=False, inplace=True)
 
     fig, ax = plt.subplots(figsize=(10, 8))
     fig.suptitle(sheet_name, fontsize=16)
-    ax.plot(df['DATA'], df['PU_BASE'], 'b')
-    ax.set_xlabel('DATA')
-    ax.set_ylabel('PU BASE')
+    ax.plot(df["DATA"], df["PU_BASE"], "b")
+    ax.set_xlabel("DATA")
+    ax.set_ylabel("PU BASE")
     ax.grid(True)
 
     if download:
         today = datetime.utcnow().strftime("%d_%m_%Y")
-        sheet_name = sheet_name.replace(' ', '_')
-        plt.savefig(f'{today}_{sheet_name}_PU_BASE.svg', format='svg', dpi=1200)
+        sheet_name = sheet_name.replace(" ", "_")
+        plt.savefig(f"{today}_{sheet_name}_PU_BASE.svg", format="svg", dpi=1200)
     plt.show()
 
 
 def get_treasure_data():
     response = requests.get(URL)
-    content = response.content.decode('utf-8')
+    content = response.content.decode("utf-8")
     soup = bs(content, "html.parser")
-    table = soup.find('table', class_='tabelaPrecoseTaxas')
+    table = soup.find("table", class_="tabelaPrecoseTaxas")
 
     pretty_table = PrettyTable()
 
-    field_names = [th.text.strip() for th in table.find('tr', class_='tabelaTitulo').find_all('th')]
+    field_names = [
+        th.text.strip() for th in table.find("tr", class_="tabelaTitulo").find_all("th")
+    ]
     pretty_table.field_names = field_names
 
-    treasure_title = [tr.find('td').text.strip() for tr in table.find_all('tr', class_='tituloprefixado')]
+    treasure_title = [
+        tr.find("td").text.strip()
+        for tr in table.find_all("tr", class_="tituloprefixado")
+    ]
     treasure_index, treasure_size = 0, len(treasure_title)
 
-    for tr in table.find_all('tr', class_='camposTesouroDireto'):
-        td_list = [td.text.strip() for td in tr.find_all('td')]
+    for tr in table.find_all("tr", class_="camposTesouroDireto"):
+        td_list = [td.text.strip() for td in tr.find_all("td")]
         title = treasure_title[treasure_index]
         if treasure_index < treasure_size and title.split()[-1][:-1] in td_list[0]:
-            pretty_table.add_row([title] + ['']*4)
+            pretty_table.add_row([title] + [""] * 4)
             treasure_index += 1
         pretty_table.add_row(td_list)
     print(pretty_table)
